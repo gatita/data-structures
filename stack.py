34e78da9e8f3b418d0a1b38e0731a1e0e365443e
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-06-25 12:36:12 -0700

    add optional iter values

--- stack.py
+++ stack.py
@@ -3,14 +3,14 @@ from __future__ import unicode_literals
 
 import linked_list as ll
 
+
 class Stack(object):
 
-    def __init__(self, values):
-        # stack has-a linked list
-        self.container = ll.LinkedList()
-
+    def __init__(self, values=None):
+        '''stack has-a linked list'''
         if values:
             self.container = ll.LinkedList(values)
+        self.container = ll.LinkedList()
 
     def push(self, value):
         self.container.insert(value)
@@ -18,14 +18,12 @@ class Stack(object):
     def pop(self):
         self.container.pop()
 
-    def __repr__(self):
-        self.container.display()
 
 if __name__ == '__main__':
+    # local tests
     mystack = Stack([1, 2, 3])
     print(mystack.container.display())
     mystack.pop()
     mystack.push(9)
     mystack.push(8)
     print(mystack.container.display())
-

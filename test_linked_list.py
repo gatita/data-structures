6a98a62f065e0475062cd06bdf0e3bb7bc88dd39
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-06-26 09:05:28 -0700

    fix some flake8 errors

--- test_linked_list.py
+++ test_linked_list.py
@@ -1,63 +1,68 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 import pytest
 from linked_list import LinkedList
+
 
 # create linked list, == empty list
 # create linked list, when passed iterable
 def test_create_list():
-    newList = LinkedList()
-    assert newList.head == None
+    mylist = LinkedList()
+    assert mylist.head is None
     list2 = LinkedList([1, 2, 3])
     assert list2.size() == 3
     assert list2.head.value == 3
 
+
 # insert to empty list
 # insert to populated list
 def test_insert_node():
-    newList = LinkedList()
-    newList.insert(1)
-    assert newList.size() == 1
-    assert newList.head.value == 1
-    newList.insert(2)
-    assert newList.size() == 2
-    assert newList.head.value == 2
+    mylist = LinkedList()
+    mylist.insert(1)
+    assert mylist.size() == 1
+    assert mylist.head.value == 1
+    mylist.insert(2)
+    assert mylist.size() == 2
+    assert mylist.head.value == 2
+
 
 # pop from empty list
 # pop from populated list
 def test_pop():
-    newList2 = LinkedList()
-    assert newList2.pop() == "The list is empty."
+    my_list = LinkedList()
+    with pytest.raises(IndexError):
+        my_list.pop()
 
-    newList = LinkedList([1, 2, 3])
-    newList.pop()
-    assert newList.size() == 2
-    assert newList.head.value == 2
+    mylist = LinkedList([1, 2, 3])
+    mylist.pop()
+    assert mylist.size() == 2
+    assert mylist.head.value == 2
+
 
 # test size function
 def test_size():
-    newList = LinkedList()
-    assert newList.size() == 0
-    newList2 = LinkedList([1, 2, 3])
-    assert newList2.size() == 3
+    mylist = LinkedList()
+    assert mylist.size() == 0
+    my_list = LinkedList([1, 2, 3])
+    assert my_list.size() == 3
+
 
 # add elements, search for non-existent val
 # search for real val
 def test_search():
-    newList = LinkedList([1, 2, 3])
-    assert newList.search(4) == None
-    assert newList.search(3).value == 3
+    mylist = LinkedList([1, 2, 3])
+    assert mylist.search(4) is None
+    assert mylist.search(3).value == 3
+
 
 # remove element
 def test_remove():
-    newList = LinkedList([1, 2, 3])
-    newList.remove(newList.search(3))
-    assert newList.head.value == 2
+    mylist = LinkedList([1, 2, 3])
+    mylist.remove(mylist.search(3))
+    assert mylist.head.value == 2
 
 
 # does display() return tuple
 def test_display():
-    newList = LinkedList([1, 2, 3])
-    assert newList.display() == (3, 2, 1)
-
+    mylist = LinkedList([1, 2, 3])
+    assert mylist.display() == (3, 2, 1)

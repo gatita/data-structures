ebaf5967e8e764df4c1bf8eb98ea77e50e89dceb
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-06-28 23:01:20 -0700

    add more tests

--- test_parenthetics.py
+++ test_parenthetics.py
@@ -5,13 +5,35 @@ from __future__ import print_function
 from parenthetics import parenthetics
 
 
+def test_empty():
+    assert parenthetics('') == 0
+
+
 def test_balanced():
     assert parenthetics('()') == 0
+
+
+def test_balanced_unicode():
+    assert parenthetics('((︵)(︶))') == 0
+    assert parenthetics('((︵)()(︶))') == 0
+    assert parenthetics('((︵()()︶))') == 0
 
 
 def test_open():
     assert parenthetics('(') == 1
 
 
+def test_open_unicode():
+    assert parenthetics('((︵)(︶))(') == 1
+    assert parenthetics('(((︵)(︶))') == 1
+    assert parenthetics('(︵(()(︶))') == 1
+
+
 def test_broken():
     assert parenthetics(')') == -1
+
+
+def test_broken_unicode():
+    assert parenthetics('((︵)(︶)))') == -1
+    assert parenthetics(')((︵)(︶))') == -1
+    assert parenthetics('((︵)()︶))') == -1

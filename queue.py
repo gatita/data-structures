ce8888859c23b41568603192dda1db613a7597c4
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-29 13:03:08 -0700

    initial commit of queue

---
+++ queue.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from linked_list import LinkedList as ll
+
+
+
+
+class Queue(object):
+    def __init__(self):
+        self.container = ll()
+        self.tail = 
+
+    def enqueue(self, val):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def size(self):
+        pass

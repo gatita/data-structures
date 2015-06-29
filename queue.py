e67e994d7657ace443eaafe9acde9100d29d52d6
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-06-29 14:03:47 -0700

    add size and fix dequeue

--- queue.py
+++ queue.py
@@ -4,11 +4,21 @@ import linked_list as ll
 
 
 class Queue(object):
+    """Container to hold nodes in a first in first out order.
+
+    Implemented enqueue(val), dequeue(), and size()"""
+
     def __init__(self):
+        """Initialize a new empty queue.
+        The queue has a linked list as its container.
+        """
+
         self.container = ll.LinkedList()
         self.tail = self.container.head
 
     def enqueue(self, val):
+        """Add a node to the end of the queue whose value is val."""
+
         new_node = ll.Node(val)
         if self.tail:
             self.tail.next = new_node
@@ -18,13 +28,19 @@ class Queue(object):
             self.container.head = new_node
 
     def dequeue(self):
-        return self.container.pop()
+        """Remove the first node from the queue and return its value."""
+
+        node_val = self.container.pop()
+
+        # reset head and tail if queue becomes empty
+        if len(self.container) == 0:
+            self.container.head = None
+            self.tail = self.container.head
+
+        return node_val
 
     def size(self):
-        pass
-
-if __name__ == '__main__':
-    q = Queue()
-    q.enqueue(5)
-    q.enqueue(7)
-    print q.container.display()
+        """Return the size of the queue.
+        Should return 0 if empty
+        """
+        return len(self.container)

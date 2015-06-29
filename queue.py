95b46ca4ea51e2efbdf45a896b92e05ed2e41992
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-29 13:23:12 -0700

    implement init, enqueue, and dequeue

--- queue.py
+++ queue.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from linked_list import LinkedList as ll
-
-
+import linked_list as ll
 
 
 class Queue(object):
     def __init__(self):
-        self.container = ll()
-        self.tail = 
+        self.container = ll.LinkedList()
+        self.tail = self.container.head
 
     def enqueue(self, val):
-        pass
+        new_node = ll.Node(val)
+        if self.tail:
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.tail = new_node
+            self.container.head = new_node
 
     def dequeue(self):
-        pass
+        return self.container.pop()
 
     def size(self):
         pass
+
+if __name__ == '__main__':
+    q = Queue()
+    q.enqueue(5)
+    q.enqueue(7)
+    print q.container.display()

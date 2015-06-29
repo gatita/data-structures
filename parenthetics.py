65e8957ed85bc93f6257c30646ff60ddcb625445
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-28 21:43:02 -0700

    move tests to separate file

--- parenthetics.py
+++ parenthetics.py
@@ -29,10 +29,3 @@ def parenthetics(string):
     except IndexError:
         return 0
     return 1
-
-
-if __name__ == '__main__':
-    print parenthetics('())')
-    print parenthetics('(())')
-    print parenthetics('(())(')
-    print parenthetics('')

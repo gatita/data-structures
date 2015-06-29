f476c086d2f5f13fcae02eb607c0112b65ce4ba0
Grace Hatamyar <hatamyarg@gmail.com>
2015-06-28 19:12:44 -0700

    add docstring

--- parenthetics.py
+++ parenthetics.py
@@ -4,6 +4,16 @@ from stack import Stack
 
 
 def parenthetics(string):
+    """
+    Takes a unicode string (text) as input and returns one of three
+    possible values:
+        1: If the string is 'open' (there are open parens that are not closed)
+        0: If the string is 'balanced' (there are an equal number of open and
+            close parentheses in the string)
+        -1: If the string is 'broken' (a closing parens has not been proceeded
+            by one that opens)
+    """
+
     my_stack = Stack()
     for char in string:
         if char == '(':

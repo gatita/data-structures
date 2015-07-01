0cd4e36bd73c4a579c3a5b3b47236a385f587e27
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-01 15:02:36 -0700

    start tests

--- test_binheap.py
+++ test_binheap.py
@@ -6,8 +6,39 @@ import pytest
 import binheap
 
 
-def test_create_heap():
-    pass
+@pytest.fixture()
+def make_heap_empty():
+    return binheap.BinaryHeap()
+
+
+@pytest.fixture()
+def make_heap_one():
+    return binheap.BinaryHeap([1])
+
+
+@pytest.fixture()
+def make_heap_three():
+    return binheap.BinaryHeap([1, 2, 3])
+
+
+def test_create_heap(make_heap_empty):
+    heap = make_heap_empty
+    assert len(heap) == 0
+    assert isinstance(heap, binheap.BinaryHeap)
+
+
+def test_create_heap_one(make_heap_one):
+    heap = make_heap_one
+    assert len(heap) == 1
+    assert heap.heap[0] == 1
+
+
+def test_create_heap_three(make_heap_three):
+    heap = make_heap_three
+    assert len(heap) == 3
+    assert heap.heap[0] == 1
+    assert heap.heap[1] == 2
+    assert heap.heap[2] == 3
 
 
 def test_push():

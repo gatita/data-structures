95664118ec42e0ffe8557a539542b70bb268e12b
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-07 12:50:35 -0700

    add docstring

--- simple_graph.py
+++ simple_graph.py
@@ -24,6 +24,8 @@ class SimpleGraph(object):
     def add_edge(self, n1, n2):
         """Add a new edge to the graph connecting 'n1' and 'n2'.
 
+        'n1' points to 'n2'
+
         If either n1 or n2 are not already present in the graph,
         they should be added.
         """
@@ -50,12 +52,16 @@ class SimpleGraph(object):
     def neighbors(self, n):
         """Return the list of all nodes connected to 'n' by edges.
 
+        'n' points to its neighbors
+
         Raises an error if 'n' is not in the graph.
         """
         pass
 
     def adjacent(self, n1, n2):
         """Return True if there is an edge connecting 'n1' and 'n2'.
+
+        'n1' points to 'n2'
 
         Raises an error if either of the supplied nodes are not in the graph.
         """

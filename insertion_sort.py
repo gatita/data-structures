acc04cda262589dbb8add550c3dd869a2e2afb68
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-04 07:06:28 -0700

    fix range

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 
 
 def insertion_sort(l):
-    for i in range(len(l) - 1):
+    for i in range(1, len(l)):
         index = i
         val = l[i]
-        while index > 0 and l[index-1] > l[index]:
-            l[i] = l[i-1]
+        while index > 0 and val < l[index-1]:
+            l[index] = l[index-1]
             index -= 1
         l[index] = val
     return l

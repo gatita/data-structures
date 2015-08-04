5a195b2130aea44b7add1d2ff8c510a036b37f56
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-04 07:18:29 -0700

    demonstrate best and worst cases

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from timeit import timeit
 
 
 def insertion_sort(l):
@@ -11,3 +12,20 @@ def insertion_sort(l):
             index -= 1
         l[index] = val
     return l
+
+
+if __name__ == '__main__':
+    best_case = range(10000)
+    worst_case = range(10000, -1, -1)
+
+    setup = 'from __main__ import insertion_sort, best_case, worst_case'
+
+    print 'best case: ', timeit(
+        'insertion_sort(best_case)',
+        setup=setup,
+        number=1)
+
+    print 'worst_case: ', timeit(
+        'insertion_sort(worst_case)',
+        setup=setup,
+        number=1)

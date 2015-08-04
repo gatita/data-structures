b147308bf138e1ca8913881650e0575212b7471b
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-04 14:29:45 -0700

    demonstrate best and worst cases

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from timeit import timeit
 
 
 def mergesort(l):
@@ -29,3 +30,39 @@ def merge(left, right):
         merged.append(right[j])
         j += 1
     return merged
+
+
+if __name__ == '__main__':
+
+    # large inputs
+    worst_case = [i for i in range(1, 10 ** 6) if i % 2] + \
+        [i for i in range(1, 10 ** 6) if not i % 2]
+    best_case = range(1, 10000)
+
+    # smaller inputs
+    s_worst_case = [i for i in range(1, 50) if i % 2] + \
+        [i for i in range(1, 50) if not i % 2]
+    s_best_case = range(1, 50)
+
+    setup1 = 'from __main__ import mergesort, worst_case, best_case'
+    setup2 = 'from __main__ import mergesort, s_worst_case, s_best_case'
+
+    print 'best case, input size = 10^6: ', timeit(
+        'mergesort(best_case)',
+        setup=setup1,
+        number=1)
+
+    print 'worst case, input size = 10^6: ', timeit(
+        'mergesort(worst_case)',
+        setup=setup1,
+        number=1)
+
+    print 'best case, input size = 50: ', timeit(
+        'mergesort(s_best_case)',
+        setup=setup2,
+        number=10)
+
+    print 'worst case, input size = 50: ', timeit(
+        'mergesort(s_worst_case)',
+        setup=setup2,
+        number=10)

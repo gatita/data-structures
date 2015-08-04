d950b3ffe0cf861ac766467f9a0d0a6f290aebf5
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-04 14:08:19 -0700

    add merge function

--- merge_sort.py
+++ merge_sort.py
@@ -13,4 +13,19 @@ def mergesort(l):
 
 
 def merge(left, right):
-    pass
+    merged = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] > right[j]:
+            merged.append(right[j])
+            j += 1
+        else:
+            merged.append(left[i])
+            i += 1
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+    return merged

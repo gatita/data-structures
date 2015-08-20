b7fe0ac868f594f6632e19ac5383e606acab4508
Grace Hatamyar <hatamyarg@gmail.com>
2015-08-19 21:20:29 -0700

    add radix sort and best and worst cases

---
+++ radix_sort.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from timeit import timeit
+import random
+import sys
+
+
+def radix_sort(list_, base=10):
+    place = 1
+    max_length = len(str(max(list_)))
+
+    for x in range(max_length):
+        output = []
+        buckets = [[] for i in range(base)]
+
+        for i in list_:
+            digit = i // place
+            buckets[digit % base].append(i)
+
+        for bucket in buckets:
+            output.extend(bucket)
+        list_ = output[:]
+        place *= base
+
+    return output
+
+
+if __name__ == '__main__':
+    """
+    Time best and worst cases for radix sort, where best case
+    is each list item is as short as possible, and worst case is
+    a list of very large inputs.
+    """
+    best_case = [random.randint(0, 9) for x in range(10000)]
+    worst_case = [random.randint(10**3, 10**4) for x in range(10000)]
+
+    setup = 'from __main__ import radix_sort, best_case, worst_case'
+
+    print 'best case: ', timeit(
+        'radix_sort(best_case)',
+        setup=setup,
+        number=100)
+
+    print 'worst case: ', timeit(
+        'radix_sort(worst_case)',
+        setup=setup,
+        number=100)
